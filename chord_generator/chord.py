97367b29c1ea7cedb53885d2c12f2bfa4b9962dd
jonbannister <jonnyb12345@gmail.com>
2016-04-27 13:43:52 +0100

    Adding some comments

--- chord_generator/chord.py
+++ chord_generator/chord.py
@@ -17,22 +17,23 @@ def generateChordsFromFrequencies(chord_frequencies, durations=None, filename=No
                             e.g. [None, None, [0.1, 0.1, 0.1, 0.4, 0.3], None]
     '''
 
-    # Some validations
+    # Validate input
     if durations and len(durations) != len(chord_frequencies):
         raise ValueError('If durations are specified, they must be specified for each chord.')
     if weights and len(weights) != len(chord_frequencies):
         raise ValueError('If per-chord weightings are specified, they must be specified for each chord (but they can be None).')
 
+    # After validation, perform some setup with inputs (TODO: maybe these could be arguments too)
     sec_durations = durations or [1.0]*len(chord_frequencies)
     sampleRate = 44100.0
     amplitude = 8000.0
     sample_durations = [int(sampleRate*duration) for duration in sec_durations]
     sine_wave = []
-
     chord_weights = []
     for (i,fs) in enumerate(chord_frequencies):
         chord_weights.append(weights[i] if (weights and weights[i]) else ([1.0/len(fs)] * len(fs)))
 
+    # Calculate the actual sine wave that creates the chord
     for (freqs, sample_size, weighting) in zip(chord_frequencies, sample_durations, chord_weights):
         for x in range(sample_size):
             sample = 0
@@ -41,7 +42,7 @@ def generateChordsFromFrequencies(chord_frequencies, durations=None, filename=No
             sine_wave.append(sample)
     logging.debug('Sine wave has been computed, saving to file.')
             
-    # Write our sine wave to the file.
+    # Write our sine wave to the .wav file.
     f = wave.open(filename or 'my_chord.wav', 'w')
     channels = 1
     amp_width = 2
@@ -53,10 +54,3 @@ def generateChordsFromFrequencies(chord_frequencies, durations=None, filename=No
         f.writeframes(struct.pack('h', (frequency*amplitude/2)))
     f.close()
     logging.debug('Save to %s complete.' % filename)
-    
-def main():
-    generateChordsFromFrequencies(
-                    [[880.0, 1100.0, 1320.0, 1760.0], [880.0, 1100.0, 1320.0, 1479.98, 1760.0], [830.609, 987.767, 1320.0, 1661.22]], 
-                    durations=[1, 1, 2], 
-                    filename='C:/Temp/helloworld3.wav', 
-                    weights=[None, [0.1, 0.1, 0.1, 0.4, 0.3], None])
